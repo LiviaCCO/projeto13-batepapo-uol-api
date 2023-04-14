ceb13754bc0538e763210078cf53a9996bbfcabe
LiviaCCO <livia.ccapistrano@gmail.com>
2023-04-13 23:09:41 -0300

    Requisito GET1

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
-
+import { MongoClient, ObjectId } from 'mongodb';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
 //conexão com o BD
 let db;
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-mongoClient.connect().then(()=>db=mongoClient.db()).catch((err)=>console.log(err.message));
+mongoClient.connect().then(()=>db=mongoClient.db()).catch((err)=>console.log(err.message)); 
 
-const PORT = 5000;
-app.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
+//ou
+/* const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try{
+    await mongoClient.connect();
+    console.log("MongoDB conectado")
+} catch (err){
+    console.log(err.message)
+}
+const db = mongoClient.db(); */
 
-const participants=[];
 
 app.post("/participants", async(req, res)=> {
     const name=req.body;
@@ -32,5 +37,17 @@ app.post("/participants", async(req, res)=> {
     } catch (err){
         res.status(500).send(err.message)
     }
-
 })
+
+app.get('/participants', async (req, res) => {
+    try {
+      const participants = await db.collection('participants').find().toArray();
+      res.send(participants);
+      } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+      }
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
